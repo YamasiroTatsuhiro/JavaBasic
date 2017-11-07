20cd94f57be1cc6a42e7558ebf89bc911869d1c8
DESKTOP-6NN06SN\YMASR-TZ <zx1919ex@gmail.com>
2017-11-07 17:08:29 +0900

    １－7まで完了。

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,24 +21,23 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
-
-
-
-
-
-
-
-
+		bo = true;
+		by = 10;
+		s = 10;
+		c = 'あ';
+		i = 100;
+		l = 999999999;
+		f = 1.22f;
+		d = 1.1;
 		// ★ それぞれの変数の中身を出力してください
-
-
-
-
-
-
-
-
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
 
 	}
 }
